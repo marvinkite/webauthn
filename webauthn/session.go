08bbcc1caa9fd731426f3dbc7908acf90f376ca3
Felix Dubrownik <felix.dubrownik@hanko.io>
2020-12-18 16:02:23 +0100

    added missing session Data

--- webauthn/session.go
+++ webauthn/session.go
@@ -5,8 +5,10 @@ import "github.com/duo-labs/webauthn/protocol"
 // SessionData is the data that should be stored by the Relying Party for
 // the duration of the web authentication ceremony
 type SessionData struct {
-	Challenge            string                               `json:"challenge"`
-	UserID               []byte                               `json:"user_id"`
-	AllowedCredentialIDs [][]byte                             `json:"allowed_credentials,omitempty"`
-	UserVerification     protocol.UserVerificationRequirement `json:"userVerification"`
+	Challenge               string                               `json:"challenge"`
+	UserID                  []byte                               `json:"user_id"`
+	AllowedCredentialIDs    [][]byte                             `json:"allowed_credentials,omitempty"`
+	UserVerification        protocol.UserVerificationRequirement `json:"userVerification"`
+	ConveyancePreference    protocol.ConveyancePreference        `json:"conveyance_preference"`
+	AuthenticatorAttachment protocol.AuthenticatorAttachment     `json:"authenticator_attachment"`
 }
